ce4188912abc593a93e87c8c2fcd269820e06db2
Jake Graham <jake.graham.95@gmail.com>
2021-03-07 12:32:02 -0500

    Move to sole repo

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI _textComponent;
+    [SerializeField] State _startingState;
+
+    State _currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentState = _startingState; 
+        _textComponent.text = _currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+	private void ManageState()
+	{
+        var nextStates = _currentState.GetNextStates();
+
+        for(int i = 0; i < nextStates.Length; i++)
+		{
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                _currentState = nextStates[i];
+            }
+        }
+        _textComponent.text = _currentState.GetStateStory();
+	}
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+	//Top level variables have underscore in front for readablility and searchablility
+	[TextArea(14,18)] [SerializeField] string _storyText;
+	[SerializeField] State[] _nextStates;
+
+	public string GetStateStory()
+	{
+		return _storyText;
+	}
+
+	public State[] GetNextStates()
+	{
+		return _nextStates;
+	}
+}
